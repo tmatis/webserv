9dc1673e4d4367fddc55d6ac90c1d22d125d3e5b
tmatis <tmatis@student.42.fr>
2021-10-11 20:24:40 +0000

    add more test

--- unit_tests/http/test_HTTPRequest.cpp
+++ unit_tests/http/test_HTTPRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 15:32:00 by tmatis            #+#    #+#             */
-/*   Updated: 2021/10/11 19:38:59 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/10/11 20:23:36 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,40 @@ car_test test_parseChunk_post_body(void)
 {
 	HTTPRequest req;
 
-	req.parseChunk("POST / HTTP/1.1\r\nHost: localhost:8080\r\nContent-Length: 4\r\n\r\nbody");
+	req.parseChunk("POST / HTTP/1.1\r\nHost: localhost:8080\r\nContent-Length: 20\r\n\r\n");
+	req.parseChunk("Hello World ");
+	req.parseChunk("!!!!!!!!");
 
 	assert(req.getMethod() == "POST");
 	assert(req.getURI() == "/");
 	assert(req.getVersion() == "HTTP/1.1");
 	assert((*req.getHeader().getValue("Host")) ==  "localhost:8080");
-	assert(req.getBody() == "body");
+	assert(req.getBody() == "Hello World !!!!!!!!");
 	assert(req.isReady() == true);
 }
 
+car_test test_parseChunk_post_body_with_multiple_chunk(void)
+{
+	HTTPRequest req;
+
+	req.parseChunk("POST / HTTP/1.1\r\nHost: localhost:8080\r\nContent-Length: 4\r\n\r\nbody");
+}
+	
+
+car_test test_parsed_get(void)
+{
+	HTTPRequest req;
+
+	req.parseChunk("GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: Mozilla/5.0\r\nAccept: text/html,text/plain\r\nConnection: keep-alive\r\n\r\n");
+	
+	assert(req.getMethod() == "GET");
+	assert(req.getURI() == "/");
+	assert(req.getVersion() == "HTTP/1.1");
+	assert(req.getHost() == "localhost:8080");
+	assert(*req.getUserAgent() == "Mozilla/5.0");
+	assert(req.getAccept()[0] == "text/html");
+	assert(req.getAccept()[1] == "text/plain");
+	assert(req.getConnection() == HTTP_CONNECTION_KEEP_ALIVE);
+	assert(req.getBody() == "");
+	assert(req.isReady() == true);
+}
