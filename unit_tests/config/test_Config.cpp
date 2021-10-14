932d05c4057ef56360e140777304954379873965
nouchata <nouchataa@gmail.com>
2021-10-14 18:08:34 +0200

    new unit tests

--- unit_tests/config/test_Config.cpp
+++ unit_tests/config/test_Config.cpp
@@ -6,43 +6,43 @@ car_test test_parseConfig(void)
 
 	mconfig.construct("unit_tests/config/webserv.cnf");
 
-	assert(mconfig._max_simultaneous_clients == 666);
+	car_assert_cmp(mconfig._max_simultaneous_clients, 666);
 
-	assert(mconfig._index_paths.size() == 1);
-	assert(mconfig._index_paths.find("index.html") \
-	!= mconfig._index_paths.end());
+	car_assert_cmp(mconfig._index_paths.size(), static_cast<unsigned long>(1));
+	car_assert_cmp(mconfig._index_paths.find("index.html") \
+	!= mconfig._index_paths.end(), true);
 
-	assert(mconfig._root == "./www/");
-	assert(mconfig._autoindex);
+	car_assert_cmp(mconfig._root, "./www/");
+	car_assert_cmp(mconfig._autoindex, true);
 
-	assert(mconfig._configs.size() == 2);
+	car_assert_cmp(mconfig._configs.size(), static_cast<unsigned long>(2));
 
-	assert(mconfig._configs[0].address == "127.0.0.1");
-	assert(mconfig._configs[0].port == htons(80));
-	assert(mconfig._configs[0].methods.size() == 1);
-	assert(mconfig._configs[0].server_names.find("localhost") \
-	!= mconfig._index_paths.end());
+	car_assert_cmp(mconfig._configs[0].address, "127.0.0.1");
+	car_assert_cmp(mconfig._configs[0].port, htons(80));
+	car_assert_cmp(mconfig._configs[0].methods.size(), static_cast<unsigned long>(1));
+	car_assert_cmp(mconfig._configs[0].server_names.find("localhost") \
+	!= mconfig._index_paths.end(), true);
 
-	assert(mconfig._configs[1].address == "0.0.0.0");
-	assert(mconfig._configs[1].port == htons(8080));
-	assert(mconfig._configs[1].methods.size() == 3);
-	assert(!mconfig._configs[1].server_names.size());
-	assert(!mconfig._configs[1]._autoindex);
+	car_assert_cmp(mconfig._configs[1].address, "0.0.0.0");
+	car_assert_cmp(mconfig._configs[1].port, htons(8080));
+	car_assert_cmp(mconfig._configs[1].methods.size(), static_cast<unsigned long>(3));
+	car_assert_cmp(mconfig._configs[1].server_names.size(), static_cast<unsigned long>(0));
+	car_assert_cmp(mconfig._configs[1]._autoindex, false);
 
 	// meant to be changed when default errors'll come
-	assert(!mconfig._configs[1]._error_pages.size());
+	car_assert_cmp(mconfig._configs[1]._error_pages.size(), static_cast<unsigned long>(0));
 
-	assert(mconfig._configs[1]._root == "./server8080/");
-	assert(mconfig._configs[1].body_limit == 650);
+	car_assert_cmp(mconfig._configs[1]._root, "./server8080/");
+	car_assert_cmp(mconfig._configs[1].body_limit, static_cast<unsigned long>(650));
 
-	assert(mconfig._configs[1].routes.size() == 2);
-	assert(mconfig._configs[1].routes[0].cgi_extension == ".php");
-	assert(mconfig._configs[1].routes[0].cgi_path == "path");
-	assert(mconfig._configs[1].routes[0].upload_path == "path");
-	assert(mconfig._configs[1].routes[0].redirection.first == 0);
-	assert(!mconfig._configs[1].routes[0]._uploadfiles);
+	car_assert_cmp(mconfig._configs[1].routes.size(), static_cast<unsigned long>(2));
+	car_assert_cmp(mconfig._configs[1].routes[0].cgi_extension, ".php");
+	car_assert_cmp(mconfig._configs[1].routes[0].cgi_path, "path");
+	car_assert_cmp(mconfig._configs[1].routes[0].upload_path, "path");
+	car_assert_cmp(mconfig._configs[1].routes[0].redirection.first, 0);
+	car_assert_cmp(mconfig._configs[1].routes[0]._uploadfiles, false);
 
-	assert(mconfig._configs[1].routes[1].cgi_extension == "");
-	assert(mconfig._configs[1].routes[1]._uploadfiles);
-	assert(mconfig._configs[1].routes[1].upload_path == "res/images/");
+	car_assert_cmp(mconfig._configs[1].routes[1].cgi_extension, "");
+	car_assert_cmp(mconfig._configs[1].routes[1]._uploadfiles, true);
+	car_assert_cmp(mconfig._configs[1].routes[1].upload_path, "res/images/");
 }
